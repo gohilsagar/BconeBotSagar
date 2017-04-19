b5041c9f4e536667e8df0296f893af136e44b37d
gohilsagar <sagar.gohil@bcone.com>
2017-04-19 15:11:18 +0530

    Created First copy of Bot Project

---
+++ app.js
@@ -0,0 +1,58 @@
+/**
+ * Created by sagar.gohil on 18-04-2017.
+ */
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: '24ac1657-cd00-4df0-9629-41a6fd555387',
+    appPassword:'YxbQYBFgU01MiKJdLHgXm0R'
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
+
+
+var http = require('http'),
+fs = require('fs');
+
+
+fs.readFile('./termsOfUse.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
+    http.createServer(function(request, response) {
+        response.writeHeader(200, {"Content-Type": "text/html"});
+        response.write(html);
+        response.end();
+    }).listen(80);
+});
+
+fs.readFile('./privacyStatement.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
+    http.createServer(function(request, response) {
+        response.writeHeader(200, {"Content-Type": "text/html"});
+        response.write(html);
+        response.end();
+    }).listen(81);
+});
